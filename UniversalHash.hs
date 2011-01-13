200881292b6e14a2cecfa318aec00d39e301507e
cullina <daniel.cullina@gmail.com>
2011-01-13 15:45:15 -0600

    Changed to use new Poly code.

--- UniversalHash.hs
+++ UniversalHash.hs
@@ -1,40 +1,30 @@
 module UniversalHash where
 
 import BinaryField
+import PrimitivePoly
+import Data.List(foldl')
+
+basicHash input aBits bBits xBits = 
+    let charPoly = getCharPoly input
+        a        = Poly input aBits
+        b        = Poly input bBits
+        x        = Poly input xBits
+    in expand input . polyBits . polySum b . polyProduct charPoly a $ x
 
 
-universalHash input output a b x = 
-    let charPoly = getCharPoly input 
-    in truncatePoly output . polySum b . polyProduct charPoly a
+alternate (a:b:c) = (a, b) : alternate c
 
-{-
-randomHash random (charPoly, outputSize, x) = let (a, b) = split (length charPoly) random
-                                              in universalHash charPoly outputSize a b x
-
-chainHash random xs = foldl randomHash random xs
--}
+alternate _ = []
 
 
+universalHash abBits xBits =
+    let input          = length xBits
+        used           = take input abBits
+        (aBits, bBits) = unzip used
+        hashedBits = basicHash input aBits bBits xBits
+    in used ++ alternate hashedBits
+        
 
-getCharPoly len
-    | len ==  2 = p2
-    | len ==  3 = p3
-    | len ==  4 = p4
-    | len ==  5 = p5
-    | len ==  6 = p6
-    | len ==  7 = p7
-    | len ==  8 = p8
+chainHash = foldl' universalHash
 
-{-
-    | len ==  9 = p9
-    | len == 10 = p10
-    | len == 11 = p11
-    | len == 12 = p12
-    | len == 13 = p13
-    | len == 14 = p14
-    | len == 15 = p15
-    | len == 16 = p16
--}  
-    | otherwise = undefined
-
-
+                     
