abb7e70192c56c1e05c8f2e89a605d9d4a6fd19c
cullina <daniel.cullina@gmail.com>
2011-01-17 17:37:15 -0600

    More progress toward whole extractor.

--- UniversalHash.hs
+++ UniversalHash.hs
@@ -5,7 +5,7 @@ import BinaryField
 import PrimitivePoly
 import SubsetSelection
 import Data.List(foldl')
-import Control.Monad(mapM)
+import Control.Monad(mapM, replicateM, liftM)
 
 
 basicHash input aBits bBits xBits = 
@@ -16,33 +16,37 @@ basicHash input aBits bBits xBits =
     in expand input . polyBits . polySum b . polyProduct charPoly a $ x
 
 
-alternate (a:b:c) = (a, b) : alternate c
-
-alternate _ = []
-
-
 universalHash abBits xBits =
     let input          = length xBits
-        used           = take input abBits
-        (aBits, bBits) = unzip used
-        hashedBits = basicHash input aBits bBits xBits
-    in used ++ alternate hashedBits
+        (aBits, rest)  = splitAt input abBits
+        bBits          = take input rest
+        hashedBits     = basicHash input aBits bBits xBits
+    in aBits ++ bBits ++ hashedBits
+
         
-
-chainHash xx@(x:xs) bs = 
+getSeed (x:xs) = 
     let seedLength  = 2 * length x
-        (seed, bs') = generateFiniteList getBit seedLength bs
-    in (foldl' universalHash (alternate seed) xx, bs')
+    in replicateM seedLength getBitM
 
-pickSubsets bits = 
-    mapM (subsetIncrementallyM (length bits)) >>= liftM (map (indicesToSubset bits))
 
-{-
-subsetSizes (x:xs) (p, q) bound =
-    let next = 
-    if 
-    
+chainHash :: [[Bool]] -> RState [Bool]
 
-extractor bits = pickSubsets bits >>= chainHash . pickSub
+chainHash xx = 
+    liftM (flip (foldl' universalHash) xx) (getSeed xx)
 
--}
+
+pickSubsets numGoodBits bits = 
+    let len           = toInteger (length bits)
+        sizes         = subsetSizes len numGoodBits
+        picker        = liftM (map (indicesToSubset bits))
+        randomSubsets = mapM (subsetIncrementallyM len)
+    in  picker (randomSubsets sizes)
+
+
+extractor numGoodBits bits = chainHash =<< pickSubsets numGoodBits bits
+
+
+
+subsetSizes :: Integer -> Integer -> [Integer]
+
+subsetSizes = undefined
