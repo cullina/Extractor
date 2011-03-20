6eb7d1a350434e54b50a571fc6354a91fc89aee3
cullina <daniel.cullina@gmail.com>
2011-03-20 18:17:00 -0500

    Both algorithm versions in one module.

--- LempelZiv.hs
+++ LempelZiv.hs
@@ -23,13 +23,13 @@ parse n (Branch x l r) _ (b:bs) =
             in (Branch x l' r, chunk', bs')
 
 
-completeParse bs = completeParse' 1 (newNode 0) bs
+completeParse p bs = completeParse' p 1 (newNode 0) bs
 
-completeParse' n tree [] = []
+completeParse' p n tree [] = []
 
-completeParse' n tree bs = 
-    let (tree', chunk, bs') = parse n tree (TerminalChunk Nothing) bs
-    in chunk:(completeParse' (n+1) tree' bs')
+completeParse' p n tree bs = 
+    let (tree', chunk, bs') = p n tree (TerminalChunk Nothing) bs
+    in chunk:(completeParse' p (n+1) tree' bs')
 
 
 bitsNeeded = bitsNeeded' 1 1 0
@@ -68,10 +68,74 @@ translate tree bs (Just (Chunk k b)) =
     translate tree (b:bs) $ safeGetValue tree k
 
 
-encodeLempelZiv = serialize . completeParse
+{-----------------------------}
+
+parse2 n t chunk [] = 
+    case t of
+      Branch x Leaf           Leaf           -> (t, TerminalChunk x, [])
+      Branch x (Branch _ _ _) (Branch _ _ _) -> (t, TerminalChunk x, [])
+      _                                      -> (t, chunk, [])
     
+
+parse2 n Leaf chunk bs = (newNode n, chunk, bs)
+
+parse2 n (Branch x l r) _ (b:bs) =
+    let chunk = case (l, r, b) of
+                  (Branch _ _ _, Leaf, True)  -> TerminalChunk x
+                  (Leaf, Branch _ _ _, False) -> TerminalChunk x
+                  _                         -> Chunk x b
+    in if b
+       then let (r', chunk', bs') = parse2 n r chunk bs
+            in (Branch x l r', chunk', bs')
+       else let (l', chunk', bs') = parse2 n l chunk bs
+            in (Branch x l' r, chunk', bs')
+
+
+deserialize2 bs = deserialize2' (append ([], Leaf) (False, False)) bitsNeeded bs
+
+deserialize2' t (m:ms) [] = []
+
+deserialize2' t (m:ms) bs =
+    let (k, bs')   = splitAt m bs
+        k'         = incrementInt k
+        inferedBit = inferOtherChild $ safeGetValue (snd t) k'
+        t'         = append t (False, False)
+    in case (inferedBit, bs') of
+         (Just iB, b:[])   -> Chunk k b : []
+         (Just iB, _)      -> Chunk k iB : deserialize2' (markChild iB t' k') ms bs'
+         (Nothing, [])     -> TerminalChunk k : []
+         (Nothing, b:bs'') -> Chunk k b : deserialize2' (markChild b t' k') ms bs''
+
+
+
+markChild b (n, tree) k 
+    = (n, modifyNode (markChild' b) tree (stripPrefixBits k))
+
+markChild' True  (Just (b, _)) = Just (b, True)
+
+markChild' False (Just (_, b)) = Just (True, b)
+    
+
+
+inferOtherChild (Just (True, False)) = Just True
+
+inferOtherChild (Just (False, True)) = Just False
+
+inferOtherChild _ = Nothing
+
+{---------------------------------------------------------}
+
+encodeLempelZiv = serialize . completeParse parse
 
 decodeLempelZiv bits = 
     let chunks = deserialize bits
+        tree   = listToTree chunks
+    in concatMap (translate tree [] . Just) chunks
+
+
+encodeLempelZiv2 = serialize . completeParse parse2
+
+decodeLempelZiv2 bits = 
+    let chunks = deserialize2 bits
         tree   = listToTree chunks
     in concatMap (translate tree [] . Just) chunks
