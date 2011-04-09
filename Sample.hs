fd0b40be569655b65a33e6594f5bcc543abdc47f
cullina <daniel.cullina@gmail.com>
2011-04-09 17:35:19 -0500

    Chart the output of integer random variables.

---
+++ Sample.hs
@@ -0,0 +1,16 @@
+module Sample where
+
+import Distribution
+import Histogram
+import Bitstream
+
+generateSamples dist n seed = 
+    let bs = stdBitstream seed
+        (samples, bs') = generateSamples' dist n [] bs 
+    in (treeHistogram samples, bs')
+
+generateSamples' dist 0 samples bs = (samples, bs)
+
+generateSamples' dist n samples bs =
+    let (x, bs') = getValue dist bs
+    in generateSamples' dist (n-1) (x:samples) bs'
