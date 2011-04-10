7ec4e74c84174d166bdd5d7592fe6918789361f3
cullina <daniel.cullina@gmail.com>
2011-04-10 17:52:02 -0500

    Beginning of Markov chain generation tool.

---
+++ Markov.hs
@@ -0,0 +1,20 @@
+module Markov where
+
+import Bitstream
+import Control.Monad.State
+
+type MarkovState a = State (a, Bitstream) a
+
+
+data Markov a = Markov (RState a) (a -> RState a)
+
+
+convertToMarkov :: (a -> RState a) -> MarkovState a
+
+convertToMarkov transition =  
+    State (duplicateFst . (uncurry (runState . transition)))
+        where duplicateFst (x,s) = (x,(x,s))
+
+
+getChain transition n =
+    replicate n (convertToMarkov transition)
