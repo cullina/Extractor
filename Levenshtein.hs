0de00ced9166c8bdb83bda5692dedfd200d3c27f
cullina <daniel.cullina@gmail.com>
2011-05-23 19:27:12 -0500

    Computes distance and counts number of edits of minimum length.

---
+++ Levenshtein.hs
@@ -0,0 +1,66 @@
+module Levenshtein where
+
+
+matchPattern (x:xs) (y:ys) = 
+    (x == y, map (== x) ys, map (== y) xs) : matchPattern xs ys
+
+matchPattern _ _ = []
+
+
+updateCosts (c1:c2:cs) (match:ms) cost =
+    let upperCost = if match
+                    then c1
+                    else c2 + 1
+        newCost = min cost upperCost
+    in if newCost > 0
+       then newCost : updateCosts (c2:cs) ms newCost
+       else repeat 0
+
+updateCosts _ [] cost = []
+
+
+
+
+
+slowLDist (x:xs) (y:ys) = 
+    if x == y
+    then slowLDist xs ys
+    else 1 + min (slowLDist xs (y:ys)) (slowLDist (x:xs) ys)
+
+
+slowLDist xs [] = length xs
+
+slowLDist [] ys = length ys
+
+
+
+quadLDist xs ys = quadLDist' xs ys (0,1) (iterate incFst (1,1))
+
+quadLDist' xs [] c costs = last costs
+
+quadLDist' xs (y:ys) c costs =
+    let matches  = map (==y) xs
+        newCosts = buildRow (incFst c) c costs matches
+    in quadLDist' xs ys (incFst c) newCosts
+
+
+
+buildRow leftCost diagonalCost (c:cs) (m:ms) =
+    let c1 = incFst (minCost leftCost c)
+        c2 = if m 
+             then minCost c1 diagonalCost
+             else c1
+    in c2 : buildRow c2 c cs ms
+    
+
+
+buildRow _ _ _ _ = []
+    
+incFst (c, n) = (c+1, n)
+
+minCost (c1, n1) (c2, n2) = 
+    case compare c1 c2 of
+      EQ -> (c1, n1 + n2)
+      LT -> (c1, n1)
+      GT -> (c2, n2)
+
