4ce7f6d6ffba970b9ad6d4c68f8ac459a6a48d3f
unknown <Dan@.(none)>
2011-06-05 17:08:24 -0400

    Split Uniform into two sections. The original contain definitions and maniputation. UniformGeneration contains functions to produce uniforms for bitstreams.

--- Uniform.hs
+++ Uniform.hs
@@ -1,15 +1,14 @@
 module Uniform 
     (
-     newUnifNat,
-     uniform, 
+     UnifNat(..),
+     newUnifNat, 
      mergeUniforms, 
      identityUnifNat,
+     addBit,
+     gcdPlus,
      extractUseful, 
-     uniformFromRecycle, 
      decision,
-     ratioDecision,
-     randomDecision,
-     efficientDecision
+     ratioDecision
     )
 where
 
@@ -24,84 +23,7 @@ data UnifNat a = UnifNat {
 newUnifNat value max =
     UnifNat (mod value max) max
 
---Uniform generation
 
-uniform = recycleUniform
-
---simple rejection sampling
-rejectUniform max bs = rejectUniform' max (maxInBits max) bs
-
-rejectUniform' max mib bs = 
-    let (x, bs') = popPush mib [] bs
-        try      = bitsToInt (reverse x)
-    in if try < max
-       then (UnifNat try max, bs')
-       else rejectUniform' max mib bs'
-
---fail fast rejection sampling
-fastRejectUniform max bs = 
-    let mib        = maxInBits max
-        (out, bs') = fastRejectUniform' mib mib [] bs
-    in (UnifNat (bitsToInt (reverse out)) max, bs')
-
-fastRejectUniform' _ [] xs bs = (xs, bs)
-
-fastRejectUniform' mib (m:ms) xs bs = 
-    let (b, bs') = getBit bs 
-    in if m
-       then if b
-            then fastRejectUniform' mib ms (b:xs) bs'   {- append and continue -}
-            else popPush ms (b:xs) bs'        {- append, continue without further checks -}
-       else if b
-            then fastRejectUniform' mib mib [] bs'      {- discard and restart -}
-            else fastRejectUniform' mib ms (b:xs) bs'   {- append and continue -}
-
-popPush [] xs bs = (xs, bs)
-
-popPush (m:ms) xs bs = 
-    let (b, bs') = getBit bs
-    in  popPush ms (b:xs) bs'
-
-
---recycle rejected portion of interval
-recycleUniform max bs = recycleUniform' max identityUnifNat bs
-
-recycleUniform' max u bs =
-    if maxValue u < max
-    then let (b, bs') = getBit bs
-         in recycleUniform' max (addBit u b) bs'
-    else case decision u max of
-           (False, u') -> (u', bs)
-           (True, u')  -> recycleUniform' max u' bs
-
-
--- False with probability num/den
-
-biasedBit den 0 bs = (True, bs)
-
-biasedBit den num bs = 
-    let (b, bs') = getBit bs
-        num'     = 2 * num
-    in if num' >= den
-       then if b
-            then biasedBit den (num' - den) bs'
-            else (b, bs')
-       else if b
-            then (b, bs')
-            else biasedBit den num' bs'
-
-uniformViaReal max bs = 
-    let mib      = maxInBits max
-        denom    = 2 ^ length mib
-        (x, bs') = popPush mib [] bs
-        try      = bitsToInt (reverse x)
-        (q, r)   = quotRem (try * max) denom
-    in if r + max <= denom
-       then (UnifNat q max, bs')
-       else let (b, bs'') = biasedBit max (denom - r) bs'
-            in if b
-               then (UnifNat (q+1) max, bs'')
-               else (UnifNat  q    max, bs'')
 
 
 --uniform manipulation
@@ -129,20 +51,7 @@ extractUseful max (UnifNat a b) =
         leftover                                = UnifNat r leftoverSize
     in (useful, stillNeeded, leftover)
 
--- more uniform generation
-{------}{------}{------}{------}{------}{------}{------}{------}{------}{------}
 
-uniformFromRecycle max randInt bs = 
-    let (useful, stillNeeded, leftover) = extractUseful max randInt
-        (newInt, bs')                   = uniform stillNeeded bs
-        merged                          = mergeUniforms newInt useful
-    in (merged, leftover, bs')   
-
-uniformFromRecycle2 max n@(UnifNat a b) bs =
-    let (usefulSize, stillNeeded, leftoverSize) = gcdPlus max b
-        (newInt, bs')                           = uniform stillNeeded bs
-        merged                                  = mergeUniforms newInt n
-    in (merged, leftoverSize, bs')
 
 
 -- decision making
@@ -159,21 +68,7 @@ ratioDecision n@(UnifNat a b) numer denom =
     let threshold = (b * numer) `div` denom
     in decision n threshold
 
-randomDecision threshold max bs =
-    let (randInt, bs') = uniform max bs
-        (d, leftover)  = decision randInt threshold
-    in (d, leftover, bs')
 
-efficientDecision threshold max randInt bs = 
-    let (merged, leftover, bs') = uniformFromRecycle max randInt bs
-        (d, leftover2)          = decision merged threshold
-        leftover3               = mergeUniforms leftover2 leftover --preserve bit ordering
-    in (d, leftover3, bs')
-
-efficientDecision2 threshold max randInt bs =
-    let (merged, leftover, bs') = uniformFromRecycle2 max randInt bs
-        (d, leftover2)          = decision merged (threshold * leftover)
-    in (d, leftover2, bs')
 
 -- nonuniform finite support random variables
 {------}{------}{------}{------}{------}{------}{------}{------}{------}{------}
---
+++ UniformGeneration.hs
@@ -0,0 +1,118 @@
+module UniformGeneration where
+
+import Bit
+import Bitstream
+import Uniform
+
+--Uniform generation
+
+uniform = recycleUniform
+
+--simple rejection sampling
+rejectUniform max bs = rejectUniform' max (maxInBits max) bs
+
+rejectUniform' max mib bs = 
+    let (x, bs') = popPush mib [] bs
+        try      = bitsToInt (reverse x)
+    in if try < max
+       then (UnifNat try max, bs')
+       else rejectUniform' max mib bs'
+
+--fail fast rejection sampling
+fastRejectUniform max bs = 
+    let mib        = maxInBits max
+        (out, bs') = fastRejectUniform' mib mib [] bs
+    in (UnifNat (bitsToInt (reverse out)) max, bs')
+
+fastRejectUniform' _ [] xs bs = (xs, bs)
+
+fastRejectUniform' mib (m:ms) xs bs = 
+    let (b, bs') = getBit bs 
+    in if m
+       then if b
+            then fastRejectUniform' mib ms (b:xs) bs'   {- append and continue -}
+            else popPush ms (b:xs) bs'        {- append, continue without further checks -}
+       else if b
+            then fastRejectUniform' mib mib [] bs'      {- discard and restart -}
+            else fastRejectUniform' mib ms (b:xs) bs'   {- append and continue -}
+
+popPush [] xs bs = (xs, bs)
+
+popPush (m:ms) xs bs = 
+    let (b, bs') = getBit bs
+    in  popPush ms (b:xs) bs'
+
+
+--recycle rejected portion of interval
+recycleUniform max bs = recycleUniform' max identityUnifNat bs
+
+recycleUniform' max u bs =
+    if maxValue u < max
+    then let (b, bs') = getBit bs
+         in recycleUniform' max (addBit u b) bs'
+    else case decision u max of
+           (False, u') -> (u', bs)
+           (True, u')  -> recycleUniform' max u' bs
+
+
+-- False with probability num/den
+
+biasedBit den 0 bs = (True, bs)
+
+biasedBit den num bs = 
+    let (b, bs') = getBit bs
+        num'     = 2 * num
+    in if num' >= den
+       then if b
+            then biasedBit den (num' - den) bs'
+            else (b, bs')
+       else if b
+            then (b, bs')
+            else biasedBit den num' bs'
+
+uniformViaReal max bs = 
+    let mib      = maxInBits max
+        denom    = 2 ^ length mib
+        (x, bs') = popPush mib [] bs
+        try      = bitsToInt (reverse x)
+        (q, r)   = quotRem (try * max) denom
+    in if r + max <= denom
+       then (UnifNat q max, bs')
+       else let (b, bs'') = biasedBit max (denom - r) bs'
+            in if b
+               then (UnifNat (q+1) max, bs'')
+               else (UnifNat  q    max, bs'')
+               
+               
+-- more uniform generation
+{------}{------}{------}{------}{------}{------}{------}{------}{------}{------}
+
+uniformFromRecycle max randInt bs = 
+    let (useful, stillNeeded, leftover) = extractUseful max randInt
+        (newInt, bs')                   = uniform stillNeeded bs
+        merged                          = mergeUniforms newInt useful
+    in (merged, leftover, bs')   
+
+uniformFromRecycle2 max n@(UnifNat a b) bs =
+    let (usefulSize, stillNeeded, leftoverSize) = gcdPlus max b
+        (newInt, bs')                           = uniform stillNeeded bs
+        merged                                  = mergeUniforms newInt n
+    in (merged, leftoverSize, bs')
+
+{------}{------}{------}{------}{------}{------}{------}{------}{------}{------}
+    
+randomDecision threshold max bs =
+    let (randInt, bs') = uniform max bs
+        (d, leftover)  = decision randInt threshold
+    in (d, leftover, bs')
+
+efficientDecision threshold max randInt bs = 
+    let (merged, leftover, bs') = uniformFromRecycle max randInt bs
+        (d, leftover2)          = decision merged threshold
+        leftover3               = mergeUniforms leftover2 leftover --preserve bit ordering
+    in (d, leftover3, bs')
+
+efficientDecision2 threshold max randInt bs =
+    let (merged, leftover, bs') = uniformFromRecycle2 max randInt bs
+        (d, leftover2)          = decision merged (threshold * leftover)
+    in (d, leftover2, bs')
