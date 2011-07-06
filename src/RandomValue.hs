58d91ff4b4b0729df8a2b834efe81b0fe4dadb9f
Daniel Cullina <daniel.cullina@gmail.com>
2011-07-06 12:47:02 -0400

    More cool tricks for working with RValues.

--- src/RandomValue.hs
+++ src/RandomValue.hs
@@ -1,7 +1,9 @@
 module RandomValue where
 
 
-data RValue b a = Done a | NotDone(b -> RValue b a)
+import Control.Monad(replicateM)
+
+data RValue b a = Done a | NotDone (b -> RValue b a)
 
 instance Functor (RValue b) where
     fmap f (Done x)    = Done (f x)
@@ -13,9 +15,45 @@ instance Monad (RValue b) where
     (Done x)    >>= g = g x
     (NotDone f) >>= g = NotDone $ \b -> f b >>= g  
     
-composeRValues :: RValue b c -> RValue a b -> RValue a c
+composeRValues2 :: RValue b c -> RValue a b -> RValue a c
     
-composeRValues rx ry = cRV ry rx ry
+composeRValues2 rx ry = cRV ry rx ry
     where cRV _  (Done x)    _           = Done x
           cRV ry (NotDone f) (Done y)    = cRV ry (f y) ry
           cRV ry (NotDone f) (NotDone g) = NotDone $ \z -> cRV ry (NotDone f) (g z) 
+          
+composeRValues :: RValue a b -> RValue b c -> RValue a c
+
+composeRValues _ (Done x) = Done x
+  
+composeRValues ry (NotDone f) = 
+  (composeRValues2 ry . f) =<< ry
+  
+
+
+arr f = NotDone $ \b -> Done (f b)
+
+
+untilSuccess :: RValue b (Maybe a) -> RValue b a
+
+untilSuccess f = maybe (untilSuccess f) Done =<< f 
+  
+untilSuccess2 = composeRValues g where
+  g = NotDone $ \b -> maybe g Done b 
+
+
+useFixedNumber n = replicateM n $ arr id
+
+pP = mapM (const (arr id))
+
+
+
+fromList (Done x) bs = (x, bs)
+
+fromList (NotDone f) (b:bs) = fromList (f b) bs
+
+fromList (NotDone _) [] = error "Reached end of list."
+
+attachCounter = aC 0
+    where aC counter (Done x)    = Done (x, counter)
+          aC counter (NotDone f) = NotDone $ \b -> aC (counter + 1) (f b)
