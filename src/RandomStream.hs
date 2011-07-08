fd6da01104931324f90f6ddcf9bdcda474f484dd
Daniel Cullina <daniel.cullina@gmail.com>
2011-07-08 16:15:58 -0400

    Added another conversion function to RandomStream.

--- src/RandomStream.hs
+++ src/RandomStream.hs
@@ -11,21 +11,31 @@ instance Functor (RStream a) where
 
 
 
+getOne :: RStream a b -> RValue a (RStream a b, b) 
+
+getOne (Out rs b) = Done (rs, b)
+getOne (In f)     = NotDone $ \a -> getOne (f a)
+
+
+pipeList :: RStream a b -> [a] -> [b]
 
 pipeList (Out rs b) as     = b : pipeList rs as
 pipeList (In f)     (a:as) = pipeList (f a) as
 pipeList (In _)     []     = error "Reached end of list."
 
 
+takeRS :: Int -> RStream a b -> RValue a [b]
+
 takeRS 0 _          = Done []
 takeRS n (Out rs b) = fmap (b :) (takeRS (n - 1) rs) 
-takeRS n (In f)     = NotDone $ \a -> takeRS n (f a)
+takeRS n (In f)     = NotDone $ \a -> takeRS n (f a) 
 
 
 markov :: (b -> RValue a b) -> RValue a b -> RStream a b
 
 markov trans (Done x) = Out (markov trans (trans x)) x
 markov trans (NotDone f) = In $ \a -> markov trans (f a)
+
 
 repeatRV :: RValue a b -> RStream a b
 
