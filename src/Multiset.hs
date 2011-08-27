a5752a5edb49e50e678e595df658a3d80f16d39c
Daniel Cullina <daniel.cullina@gmail.com>
2011-08-26 19:13:10 -0500

    Tools for working with multisets.

---
+++ src/Multiset.hs
@@ -0,0 +1,32 @@
+module Multiset where
+
+import SubsetSelection
+import Data.List(unfoldr)
+-- multisets/compositions
+
+toList (x,xs) = x:xs
+
+fromList []     = Nothing
+fromList (x:xs) = Just (x, xs)
+
+subsetToComposition = foldr sTC (0, [])
+  where sTC True  (n, ns) = (0, n:ns)
+        sTC False (n, ns) = (n+1, ns)
+                      
+compositionToSubset = unfoldr cTS
+  where cTS (0,[])     = Nothing
+        cTS (0,(n:ns)) = Just (True,  (n,   ns))
+        cTS (n,ns)     = Just (False, (n-1, ns))
+
+subsetToSpanningComposition = foldr sTSC (1, [])
+  where sTSC True  (n, ns) = (1, n:ns)
+        sTSC False (n, ns) = (n+1, ns)
+                      
+spanningCompositionToSubset = unfoldr sCTS
+  where sCTS (1,[])     = Nothing
+        sCTS (1,(n:ns)) = Just (True,  (n,   ns))
+        sCTS (n,ns)     = Just (False, (n-1, ns))
+
+equalityToInequality (x, xs) = (xs, x + sum xs)
+
+inequalityToEquality (xs, bound) = (bound - sum xs, xs)
