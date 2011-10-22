66cb8d4f614bebc7edc72a37aadf966bfba9ef3b
Daniel Cullina <daniel.cullina@gmail.coma>
2011-10-22 14:13:46 -0500

    Fixed bugs caused by reversal of parity of UnifNat's decision. Added function to generate all permutations and function to permute a list.

--- src/Permutation.hs
+++ src/Permutation.hs
@@ -3,22 +3,24 @@ module Permutation where
 import Uniform
 import Falling
 import SubsetSelection
+import Util(mapPair)
+import Data.Function(on)
+import Data.List(sortBy, partition)
 
 slowPerm []     = []
 slowPerm (u:us) = u : map (bumpUp u) (slowPerm us)
   
 slowUnperm _ []     = []
 slowUnperm m (p:ps) = 
-  p : mapM (bumpDown p) (slowUnperm (m - 1) ps)
+  p : map (bumpDown p) (slowUnperm (m - 1) ps)
     
 bumpUp c x 
   | x >= c    = x + 1 
   | otherwise = x
 
 bumpDown c x 
-  | x > c     = Just $ x - 1 
-  | x == c    = Nothing
-  | otherwise = Just x
+  | x > c     = x - 1 
+  | otherwise = x
 
 replace x y z
   | x == z    = y
@@ -30,23 +32,21 @@ perm [] = []
 perm [UnifNat 1 0] = [0]
 perm f@(UnifNat m _ : _) =
   let split = div m 2
-      (bs, xs, ys) = disect f split
-  in unpartition bs (map (split +) (perm xs)) (perm ys)
+      (bs, (xs, ys)) = disect f split
+  in unpartition bs (perm xs) (map (split +) (perm ys))
 
-
-
+--disect proves n! = \binom{n}{k} k! (n-k)!
 disect us split = 
   let ds = decisions us split
-      f = map fst
-      g = map snd . filter fst
-      h = map snd . filter (not . fst)
-  in (f ds, g ds, h ds)
+      f  = map fst
+      g  = mapPair (map snd) . partition fst
+  in (f ds, g ds)
 
 
 decisions [] _ = []
 decisions (u:us) split = 
   let (b, v) = decision split u  
-  in (b, v) : decisions us (if b then split else (split - 1))
+  in (b, v) : decisions us (if b then (split - 1) else split)
 
 
 cyclePerm _ []     = []
@@ -57,3 +57,9 @@ permToFn :: (Integral a) => [a] -> a -> Maybe a
 permToFn []    _  = Nothing
 permToFn (n:_)  0 = Just n
 permToFn (_:ns) m = permToFn ns (m-1) 
+
+permute :: (Integral a) => [a] -> [b] -> [b]
+permute p = map snd . sortBy (compare `on` fst) . zip p
+
+allPermutations :: Int -> [[Int]]
+allPermutations n = map perm $ allFallings n n
