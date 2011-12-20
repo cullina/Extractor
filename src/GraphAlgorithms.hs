863a5f0772c5bd19ba3ba9a3151af94a8e8a03cd
Daniel Cullina <daniel.cullina@gmail.com>
2011-12-19 21:23:44 -0600

    More tests.

--- src/GraphAlgorithms.hs
+++ src/GraphAlgorithms.hs
@@ -23,55 +23,15 @@ keepNeighbors (v:vs) ((x,ys):es) =
     GT -> keepNeighbors (v:vs) es
 
 partitionNeighbors :: Ord a => [a] -> [(a,[a])] -> ( [(a,[a])] , [(a,[a])] )
-partitionNeighbors ns = partition . pN ns
-  where
-    pN ns [] = []
-    pN [] es = map ((,) False) es
-    pN (n:ns) ((x,ys):es) =
-      case compare n x of 
-        LT -> pN ns ((x,ys):es)
-        EQ -> (True, (x,ys))  : pN ns es
-        GT -> (False, (x,ys)) : pN (n:ns) es
-      
-
-partitionNeighbors2 :: Ord a => [a] -> [(a,[a])] -> ( [(a,[a])] , [(a,[a])] )
-partitionNeighbors2 = pN
-  where
-    pN ns [] = ([],[])
-    pN [] es = ([],es)
-    pN (n:ns) ((x,ys):es) =
-      case compare n x of 
-        LT -> pN ns ((x,ys):es)
-        EQ -> mapFst ((x,ys) :) (pN ns es)
-        GT -> mapSnd ((x,ys) :) (pN (n:ns) es)
+partitionNeighbors ns [] = ([],[])
+partitionNeighbors [] es = ([],es)
+partitionNeighbors (n:ns) ((x,ys):es) =
+  case compare n x of 
+    LT -> partitionNeighbors ns ((x,ys):es)
+    EQ -> mapFst ((x,ys) :) (partitionNeighbors ns es)
+    GT -> mapSnd ((x,ys) :) (partitionNeighbors (n:ns) es)
 
 
-
-findAll :: Ord a => ([a] -> [(a,[a])] -> [(a,[a])]) -> [(a,[a])] -> [[a]]
-findAll f [] = [[]]
-findAll f ((x,ys):g) = 
-  map (x :) (findAll f (f ys g)) ++ findAll f g
-  
-allCliques :: Ord a => [(a,[a])] -> [[a]]
-allCliques = removeSubsets . findAll keepNeighbors
-
-allIndepSets :: Ord a => [(a,[a])] -> [[a]]
-allIndepSets = removeSubsets . findAll removeNeighbors
-
-maxCliqueSize :: Ord a => Int -> [(a,[a])] -> Int
-maxCliqueSize n = maximum . take n . map length . removeSubsets . findAll keepNeighbors
-
-maxIndepSets :: Ord a => FwdAdj a -> [[a]]
-maxIndepSets = f . fromFwdAdj 
-  where 
-    f []         = [[]]
-    f ((x,ys):g) = 
-      let is = map (x :) (f (removeNeighbors ys g))
-      in if length ys > 1
-         then is ++ f g
-         else is
-          
-          
 maxCliques :: Ord a => FwdAdj a -> [[a]]
 maxCliques = f . fromFwdAdj
   where
@@ -82,36 +42,13 @@ maxCliques = f . fromFwdAdj
       in if length rest > 1
          then is ++ f g
          else is
-              
-          
-maxCliques2 :: Ord a => FwdAdj a -> [[a]]
-maxCliques2 = f . fromFwdAdj
+
+maxIndepSets :: Ord a => FwdAdj a -> [[a]]
+maxIndepSets = f . fromFwdAdj
   where
     f []         = [[]]
     f ((x,ys):g) = 
-      let kept = keepNeighbors ys g
-          is = map (x :) (f kept)
-      in if length g - length kept > 1
-         then is ++ f g
-         else is
-              
-maxCliques3 :: Ord a => FwdAdj a -> [[a]]
-maxCliques3 = f . fromFwdAdj
-  where
-    f []         = [[]]
-    f ((x,ys):g) = 
-      let kept = keepNeighbors ys g
-          is = map (x :) (f kept)
-      in if length (removeNeighbors ys g) > 1
-         then is ++ f g
-         else is
-              
-maxCliques4 :: Ord a => FwdAdj a -> [[a]]
-maxCliques4 = f . fromFwdAdj
-  where
-    f []         = [[]]
-    f ((x,ys):g) = 
-      let (kept, rest) = partitionNeighbors2 ys g
+      let (rest, kept) = partitionNeighbors ys g
           is = map (x :) (f kept)
       in if length rest > 1
          then is ++ f g
--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,19 @@
 module Main where
 
 import Graph
-import GraphAlgorithms(allCliques, maxCliqueSize, maxCliques)
-import LevGraphs(levIntEdges,levLevelTwoEdges)
+import GraphAlgorithms(maxCliques)
+import LevGraphs(levIntEdges, levLevelTwoEdges, hWeight, levEdges)
 import LevTests(countCliqueVTH')
 import Util(argMaximumsSoFar)
 import System.Environment(getArgs)
 
 main = do
   [k,n] <- fmap (map read) getArgs
-  f k n
+  testTwo k n
 
 testOne = mapM print $ concatMap countCliqueVTH' [2..10]
 
---testTwo = mapM print . allCliques . adjList $ levIntEdges 2 5
+testTwo k = g . induceSubgraphByTest ((k >=) . hWeight) . levEdges 2
 
 testThree = mapM (f 3) [6..12]
 
@@ -21,4 +21,7 @@ testFour = mapM (\x -> f x (2*x)) [2..5]
 
 testFive = f 4 10
 
-f k = mapM print . argMaximumsSoFar length . maxCliques . adjListByDeg . levLevelTwoEdges k
+f k = g . levLevelTwoEdges k
+
+g :: (Ord a) => EdgeList a -> IO [()]
+g = mapM print . argMaximumsSoFar length . maxCliques . adjListByDeg
