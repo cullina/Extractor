70b7b64ff71da9d145236e1485649d48c918a8a3
Daniel Cullina <daniel.cullina@gmail.coma>
2012-01-19 21:42:39 -0600

    New greedy independent set search

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Graph
-import GraphAlgorithms(maxCliques, maxIndepSets)
+import GraphAlgorithms(maxCliques, maxIndepSets, greedyIndepSet, greedyClique)
 import LevGraphs(levIntEdges, levLevelIntEdges, levLevelTwoIntEdges, hWeight, levEdges, vtZeroEdges, vtZeroLevelEdges)
 import LevTests(countCliqueVTH')
 import Util(argMaximumsSoFar)
@@ -9,8 +9,9 @@ import System.Environment(getArgs)
 
 main = do
   [m,g,k,n] <- fmap (map read) getArgs
-  test m $ gen g k n
+  mapM_ (test m) $ gen g k n
 
+gen :: Int -> Int -> Int -> [EdgeList Int]
 gen 0 k n = map (levIntEdges k) [1..n]
 gen 1 k n = map (levLevelIntEdges k) [k+1..n]
 gen 2 k n = map (\x -> levLevelIntEdges x (2*x)) [1..n]
@@ -22,16 +23,18 @@ gen 7 k n = map vtZeroEdges [2..n]
 gen 8 k n = map (levIntEdges k) [n]
 gen 9 k n = map vtZeroLevelEdges [2..n]
 
-test 0 = mapM (print . length . fromEdgeList)
-test 1 = mapM (print . maxDegree . adjListFull)
-test 2 = mapM (print . minDegree . adjListFull)
-test 3 = mapM (print . degeneracy . adjListFull)
-test 4 = mapM (print . maximum . map length . maxCliques . adjList)
-test 5 = mapM print . concatMap (argMaximumsSoFar length . maxCliques . adjList)
-test 6 = mapM (print . maximum . map length . maxIndepSets . adjList)
-test 7 = mapM print . concatMap (argMaximumsSoFar length . maxIndepSets . adjList)
-test 8 = mapM (print . length . fromFullAdj . adjListFull)
 
+test :: Int -> EdgeList Int -> IO () 
+test 0 = print . length . fromEdgeList
+test 1 = print . maxDegree . adjListFull
+test 2 = print . minDegree . adjListFull
+test 3 = print . degeneracy . adjListFull
+test 4 = print . maximum . map length . maxCliques . adjList
+test 5 = mapM_ print . argMaximumsSoFar length . maxCliques . adjList
+test 6 = print . maximum . map length . maxIndepSets . adjList
+test 7 = mapM_ print . argMaximumsSoFar length . maxIndepSets . adjList
+test 8 = print . length . fromFullAdj . adjListFull 
+test 9 = print . length . greedyIndepSet . adjList
 
 
 
