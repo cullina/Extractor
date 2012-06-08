f6fc9889772df0125e4fb9dc2844edefa6ec6010
Daniel Cullina <daniel.cullina@gmail.coma>
2012-06-07 22:44:38 -0500

    Changed substring statistics to use finite representations of power series and new 2x2 matrix data type.

--- src/SubstringCounting.hs
+++ src/SubstringCounting.hs
@@ -1,16 +1,8 @@
 module SubstringCounting where
 
-import PowerSeries(seriesMult, seriesAdd, seriesOne, seriesZero)
+import PowerSeries(EOPS(..))
+import Matrix
 import Data.Monoid
-
-data SubStats = SS ((Bool,Bool) -> [Int])
-
-instance Show SubStats where
-  show = show . smallStats 4
-
-instance Monoid SubStats where
-  mempty = SS $ lookupBB (seriesOne, seriesZero, seriesZero, seriesOne)
-  mappend (SS x) (SS y) = SS $ evenOddMult x y
 
 data InvStats = IS Int Int Int
                 deriving (Show, Eq, Ord)
@@ -20,15 +12,15 @@ instance Monoid InvStats where
   mappend (IS a b c) (IS d e f) = IS (a + d) (b + e) (c + f + (b * d))
   
   
-bStats :: Bool -> SubStats
-bStats False = SS $ lookupBB (seriesOne, seriesOne, seriesZero, seriesOne)
-bStats True  = SS $ lookupBB (seriesOne, seriesZero, seriesOne, seriesOne)
+bStats :: Bool -> BMat EOPS
+bStats False = BMat $ lookupBB (Even [1], Odd [1], Zero, Even [1])
+bStats True  = BMat $ lookupBB (Even [1], Zero, Odd [1], Even [1])
 
-bsStats :: [Bool] -> SubStats
+bsStats :: [Bool] -> BMat EOPS
 bsStats = mconcat . map bStats
 
-smallStats :: Int -> SubStats -> ([Int],[Int],[Int],[Int])
-smallStats n (SS x) = expandBB $ take n . x
+diffStats :: BMat EOPS -> EOPS
+diffStats (BMat m) = (transpose diffVec `bbMatrixMult` m  `bbMatrixMult` sumVec) ((),())
 
 bInvStats :: Bool -> InvStats
 bInvStats False = IS 1 0 0
@@ -39,28 +31,3 @@ bsInvStats = mconcat . map bInvStats
 
 inversions :: InvStats -> Int
 inversions (IS _ _ n) = n
-
-lookupBB :: (a, a, a, a) -> (Bool,Bool) -> a
-lookupBB (a,_,_,_) (False, False) = a
-lookupBB (_,b,_,_) (False, True)  = b
-lookupBB (_,_,c,_) (True,  False) = c
-lookupBB (_,_,_,d) (True,  True)  = d
-
-expandBB :: ((Bool, Bool) -> a) -> (a,a,a,a)
-expandBB f = (f (False,False), f (False,True), f (True,False), f (True,True))
-
-
-evenOddEntryMult :: ((Bool,Bool) -> [Int]) -> ((Bool,Bool) -> [Int]) -> Bool -> Bool -> Bool -> [Int]
-evenOddEntryMult a b False False True  = 0 : entryMult a b False False True
-evenOddEntryMult a b True  True  False = 0 : entryMult a b True  True  False
-evenOddEntryMult a b i j k             = entryMult a b i j k
-
-entryMult :: ((Bool,Bool) -> [Int]) -> ((Bool,Bool) -> [Int]) -> Bool -> Bool -> Bool -> [Int]
-entryMult a b i j k = seriesMult (a (i, k)) (b (k, j)) 
-
--- Multiplies matrices of this form
--- ( A(z^2)   zB(z^2) ) 
--- (zC(z^2)    D(z^2) ) 
-evenOddMult :: ((Bool,Bool) -> [Int]) -> ((Bool,Bool) -> [Int]) -> (Bool,Bool) -> [Int]
-evenOddMult a b (i, j) = seriesAdd (f False) (f True)
-  where f = evenOddEntryMult a b i j
